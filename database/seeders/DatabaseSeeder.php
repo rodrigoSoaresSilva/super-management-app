d7907997858c8ff4cd5f301c2689a4d9dfff6a4b
Rodrigo Soares da Silva <guigocore@gmail.com>
2023-10-15 08:59:42 -0300

    Refactor: obtendo motivo contato diretamente do banco

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -21,5 +21,6 @@ class DatabaseSeeder extends Seeder
 
         $this->call(FornecedorSeeder::class);
         $this->call(SiteContatoSeeder::class);
+        $this->call(MotivoContatoSeeder::class);
     }
 }
--- resources/views/site/layouts/_components/form_contato.blade.php
+++ resources/views/site/layouts/_components/form_contato.blade.php
@@ -9,16 +9,15 @@
     <br>
     <select name="motivo_contato" class="{{ $classe }}">
         <option value="">Qual o motivo do contato?</option>
-        @foreach ($motivo_contatos as $key => $motivo_contato)
-            <option value="{{ $key }}" {{ old('motivo_contato') == $key ? 'selected' : '' }}>
-                {{ $motivo_contato }}
+        @foreach ($motivo_contatos as $motivo_contato)
+            <option value="{{ $motivo_contato->id }}"
+                {{ old('motivo_contato') == $motivo_contato->id ? 'selected' : '' }}>
+                {{ $motivo_contato->motivo_contato }}
             </option>
         @endforeach
     </select>
     <br>
-    <textarea name="mensagem" class="{{ $classe }}">
-        {{ old('mensagem') != '' ? old('mensagem') : 'Preencha aqui a sua mensagem' }}
-    </textarea>
+    <textarea name="mensagem" class="{{ $classe }}">{{ old('mensagem') != '' ? old('mensagem') : 'Preencha aqui a sua mensagem' }}</textarea>
     <br>
     <button type="submit" class="{{ $classe }}">ENVIAR</button>
 </form>
