2ee7acdba1b2a88109774511a62f3e0e5aa3708f
Rodrigo Soares da Silva <guigocore@gmail.com>
2024-12-30 09:22:22 -0300

    Estudando colunas pivô: withPivot

--- resources/views/app/pedido_produto/create.blade.php
+++ resources/views/app/pedido_produto/create.blade.php
@@ -28,6 +28,7 @@
                         <tr>
                             <th>ID</th>
                             <th>Nome do produto</th>
+                            <th>Data de inclusão do item no pedido</th>
                         </tr>
                     </thead>
                     <tbody>
@@ -35,6 +36,7 @@
                             <tr>
                                 <td>{{ $produto->id }}</td>
                                 <td>{{ $produto->nome }}</td>
+                                <td>{{ $produto->pivot->created_at->format('d/m/Y') }}</td>
                             </tr>
                         @endforeach
                     </tbody>
